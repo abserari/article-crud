a9fa36ee78b1ceaae1ff4976d7ec322e97472de8
Oiar <643921472@qq.com>
2020-02-16 01:05:17 +0800

    modify

--- article/mysql/article.go
+++ article/mysql/article.go
@@ -42,73 +42,73 @@ var (
 		`UPDATE %s SET text = ? , updated = ? WHERE articleId = ? LIMIT 1`,
 		`SELECT * FROM %s WHERE articleId = ? LIMIT 1 LOCK IN SHARE MODE`,
 	}
+)
 
-	//createDB
-	func CreateDB(db sql*DB, createDB string) error {
-		sql := fmt.Sprintf(articleSQLstring[mysqlArticleCreateDB], createDB)
-		_, err := db.Exec(sql)
-		return err
+//createDB
+func CreateDB(db *sql.DB, createDB string) error {
+	sql := fmt.Sprintf(articleSQLstring[mysqlArticleCreateDB], createDB)
+	_, err := db.Exec(sql)
+	return err
+}
+
+//createTable
+func CreateTable(db *sql.DB, tableName string) error {
+	sql := fmt.Sprintf(articleSQLstring[mysqlArticleCreateTable], tableName)
+	_, err := db.Exec(sql)
+	return err
+}
+
+//insertArticle
+func InsertArticle(db *sql.DB, tableName string, articleName string, author string, text string) (int, error) {
+	sql := fmt.Sprintf(articleSQLstring[mysqlArticleInsert], tableName)
+	result, err := db.Exec(sql, articleName, author, text)
+	if err != nil {
+		return 0, err
 	}
 
-	//createTable
-	func CreateTable(db *sql.DB, tableName string) error {
-		sql := fmt.Sprintf(articleSQLstring[mysqlArticleCreateTable], tableName)
-		_, err := db.Exec(sql)
-		return err
+	if rows, _ := result.RowsAffected(); rows == 0 {
+		return 0, errInvalidInsert
 	}
 
-	//insertArticle
-	func InsertArticle(db *sql.DB, tableName string, articleName string, author string, text string) (int, error) {
-		sql := fmt.Sprintf(articleSQLstring[mysqlArticleInsert], tableName)
-		result, err := db.Exec(sql, articleName, author, text)
-		if err != nil {
-			return 0, err
-		}
-
-		if rows, _ := result.RowsAffected(); rows == 0 {
-			return 0, errInvalidInsert
-		}
-
-		articleId, err := result.LastInsertId()
-		if err != nil {
-			return 0, err
-		}
-
-		return int(articleId), nil
+	articleId, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
 	}
 
-	//deleteArticleByID
-	func DeleteArticleByID(db *sql.DB, tableName string, id int) error {
-		sql := fmt.Sprintf(articleSQLstring[mysqlArticleDeleteByID], tableName)
-		_, err := db.Exec(sql, id)
-		return err
+	return int(articleId), nil
+}
+
+//deleteArticleByID
+func DeleteArticleByID(db *sql.DB, tableName string, id int) error {
+	sql := fmt.Sprintf(articleSQLstring[mysqlArticleDeleteByID], tableName)
+	_, err := db.Exec(sql, id)
+	return err
+}
+
+//updateArticleByID
+func UpdateArticleByID(db *sql.DB, tableName string, id int) error {
+	sql := fmt.Sprintf(articleSQLstring[mysqlArticleUpdateByID], tableName)
+	_, err := db.Exec(sql, id)
+	return err
+}
+
+//queryArticleByID
+func QueryArticleByID(db *sql.DB, tableName string, id int) (*Article error) {
+	var art Article
+
+	sql: = fmt.Sprintf(articleSQLstring[mysqlArticleUpdateByID], tableName)
+	rows, err := db.Query(sql, id)
+	if err != nil {
+		return nil, err
 	}
 
-	//updateArticleByID
-	func UpdateArticleByID(db *sql.DB, tableName string, id int) error {
-		sql: = fmt.Sprintf(articleSQLstring[mysqlArticleUpdateByID], tableName)
-		_, err := db.Exec(sql, id)
-		return err
-	}
+	defer rows.Close()
 
-	//queryArticleByID
-	func QueryArticleByID(db *sql.DB, tableName string, id int) (*Article error) {
-		var art Article
-
-		sql: = fmt.Sprintf(articleSQLstring[mysqlArticleUpdateByID], tableName)
-		rows, err := db.Query(sql, id)
-		if err != nil {
+	for rows.Next() {
+		if err := rows.Scan(&art.ArticleID, &art.ArticleName, &art.Author, &art.Text); err != nil {
 			return nil, err
 		}
+	}
 
-		defer rows.Close()
-
-		for rows.Next() {
-			if err := rows.Scan(&art.ArticleID, &art.ArticleName, &art.Author, &art.Text); err != nil {
-				return nil, err
-			}
-		}
-
-		return &art, nil
-	}	
-)
+	return &art, nil
+}
